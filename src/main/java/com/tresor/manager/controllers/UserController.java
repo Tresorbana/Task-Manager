51e54088e40c9e2503aed1909bb83d8ec00a4473
entelonofficial <entelonofficial@gmail.com>
2025-09-24 21:44:48 +0200

    Continue

--- src/main/java/com/tresor/manager/controllers/UserController.java
+++ src/main/java/com/tresor/manager/controllers/UserController.java
@@ -1,9 +1,4 @@
 package com.tresor.manager.controllers;
-import models.Task;
-import java.util.List;
 public class UserController {
-    private final TaskService taskService;
-    public UserController(TaskService taskService){
-        this.taskService = taskService;
-    }
+
 }
--- src/main/java/com/tresor/manager/services/TaskServices.java
+++ src/main/java/com/tresor/manager/services/TaskServices.java
@@ -1,4 +1,45 @@
 package com.tresor.manager.services;
 
+import com.tresor.manager.models.Status;
+import com.tresor.manager.models.Task;
+import com.tresor.manager.repositories.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TaskServices {
+    private final TaskRepository taskRepository;
+    public TaskServices(TaskRepository taskRepository){
+     this.taskRepository = taskRepository;
+    }
+    public List<Task> getAllTasks(){
+     return taskRepository.findAll();
+    }
+    public Task getTaskById(Long id){
+        return taskRepository.findById(id).orElse(null);
+    }
+    public Task createTask(Task task){
+        return taskRepository.save(task);
+    }
+    public Task updateTaskStatus(Long id, Status status){
+        Task task = taskRepository.findById(id).orElse(null);
+        if(task != null){
+            Task updated = new Task(
+                    task.taskId(),
+                    task.userId(),
+                    task.title(),
+                    task.description(),
+                    status,
+                    task.priority(),
+                    task.category(),
+                    task.comments(),
+                    task.dueDate(),
+                    task.createdAt()
+            );
+            taskRepository.save(updated);
+            return updated;
+        }
+        return null;
+    }
 }
